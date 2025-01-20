dafc01d80b5bcaab97e9e0360cd2441c6846facc
Jacob Lindholm <lindholmjacob@gmail.com>
2025-01-19 16:06:39 -0800

    Added a function to count characters

--- main.py
+++ main.py
@@ -1,3 +1,23 @@
+#create a function that takes a string
+#return the number of times each character appears
+#create a dictionary of the values as String : Int
+def character_count(string):
+    characters_dict = {}
+    print(string.count('p'))
+    print(chr(0x61))
+    #adds counts for every alphabet lower case char using ascii vals.
+    for i in range(0,26):
+        characters_dict[chr(0x61+i)] = string.count(chr(0x61+i))
+    #adds the punctuation counts to the dictionary
+    space = " "
+    period = "."
+    questionMark = "?"
+    characters_dict[space] = string.count(space)
+    characters_dict[period] = string.count(period)
+    characters_dict[questionMark] = string.count(questionMark)
+    print(characters_dict)
+    return characters_dict
+
 def word_count(words):
     count = 0
     count = len(words.split())
@@ -8,5 +28,6 @@ def main():
         book_contents = book.read()
     #print(book_contents)
     print(word_count(book_contents))
-    
+    character_count(book_contents.lower())
+
 main()
