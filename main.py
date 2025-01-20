545ab81aaf28f9ec652bcbc7962baaaf1210de8b
Jacob Lindholm <lindholmjacob@gmail.com>
2025-01-19 17:29:53 -0800

    Added functionality to create a report

--- main.py
+++ main.py
@@ -1,22 +1,48 @@
+def get_word_count(document):
+    count = document.split()
+    return len(count)
+
+def create_list(document):
+    doc_list = []
+    for key, value in document.items():
+        temp_dict = {"key": key, "value": value}
+        doc_list.append(temp_dict)
+    return doc_list
+
+def sort_on_key(dict):
+    return dict["value"]
+
+def sort_doc(doc_list):
+    doc_list.sort(reverse=True,key=sort_on_key)
+    return doc_list
+
+
+def print_report(file_name, document, word_count):
+    print(f"--- Begin report of {file_name} ---")
+    print(f"{word_count} words found in the document")
+    doc_list = create_list(document)
+    sorted_doc = sort_doc(doc_list)
+    key = "key"
+    value = "value"
+    for val in sorted_doc:
+        if val["key"].isalpha():
+            print(f"The '{val[key]}' character was found {val[value]} times")
+    
+    print("--- End report ---")
+
+    #print(f"The '{key}' character was found {value}")
+
 #create a function that takes a string
 #return the number of times each character appears
 #create a dictionary of the values as String : Int
-def character_count(string):
-    characters_dict = {}
-    print(string.count('p'))
-    print(chr(0x61))
-    #adds counts for every alphabet lower case char using ascii vals.
-    for i in range(0,26):
-        characters_dict[chr(0x61+i)] = string.count(chr(0x61+i))
-    #adds the punctuation counts to the dictionary
-    space = " "
-    period = "."
-    questionMark = "?"
-    characters_dict[space] = string.count(space)
-    characters_dict[period] = string.count(period)
-    characters_dict[questionMark] = string.count(questionMark)
-    print(characters_dict)
-    return characters_dict
+def get_chars_dict(text):
+    chars_dict = {}
+    for character in text:
+        if character in chars_dict:
+            chars_dict[character] += 1
+        else:
+            chars_dict[character] = 1
+    return chars_dict
 
 def word_count(words):
     count = 0
@@ -24,10 +50,10 @@ def word_count(words):
     return count
 
 def main():
-    with open("books/frankenstein.txt") as book:
+    file_name = "books/frankenstein.txt"
+    with open(file_name) as book:
         book_contents = book.read()
-    #print(book_contents)
-    print(word_count(book_contents))
-    character_count(book_contents.lower())
-
+    document_dict = get_chars_dict(book_contents.lower())
+    word_count = get_word_count(book_contents)
+    print_report(file_name, document_dict, word_count)
 main()
